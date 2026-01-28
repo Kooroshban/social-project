3e2059c8d6d373cdaadeaa55d23a0e3922f7c3cd
Kooroshban <kooroshbanyadm@gmail.com>
2026-01-29 00:59:50 +0330

    .

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const btnEl = document.getElementById("btn");
+const menuEl = document.getElementById("menu");
+const FacebookEl = document.getElementById("Facebook");
+const YouTubeEl = document.getElementById("YouTube");
+const TwitterEl = document.getElementById("Twitter");
+const LinkedinEl = document.getElementById("Linkedin");
 
-setupCounter(document.querySelector('#counter'))
+btnEl.addEventListener("click", () => {
+  menuEl.classList.toggle("hiden");
+});
+FacebookEl.addEventListener("click", () => {
+  btnEl.innerText = FacebookEl.innerText;
+  menuEl.classList.remove("hiden");
+});
+YouTubeEl.addEventListener("click", () => {
+  btnEl.innerText = YouTubeEl.innerText;
+  menuEl.classList.remove("hiden");
+});
+TwitterEl.addEventListener("click", () => {
+  btnEl.innerText = TwitterEl.innerText;
+  menuEl.classList.remove("hiden");
+});
+LinkedinEl.addEventListener("click", () => {
+  btnEl.innerText = LinkedinEl.innerText;
+  menuEl.classList.remove("hiden");
+});
